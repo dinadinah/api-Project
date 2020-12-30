908d6b8d47e3fdbf0cb14cf169449a6feaf376f4
dinadinah <diyakubova@gmail.com>
2020-12-29 22:37:00 -0500

    change

---
+++ public/index.js
@@ -0,0 +1,5 @@
+let baseURL = 'https://alexwohlbruck.github.io/cat-facts/';
+
+let quoteContainer = document.querySelector('.quoteContainer');
+let logo = document.getElementById('ronLogo');
+logo.addEventListener('click', fetchQuote);
